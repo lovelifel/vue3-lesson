abe5d2eb82411dcab9c53a6e44b1a1e4fc8ba2f7
LinYunLong <linyunlong@smartens-tek.com>
2026-01-21 17:25:46 +0800

    feat:dom

--- packages/runtime-dom/src/index.ts
+++ packages/runtime-dom/src/index.ts
@@ -1,1 +1,10 @@
-export {};
+export * from "@vue/reactivity";
+import nodeOps from "./nodeOps";
+import patchProp from "./patchProp";
+
+function createRenderer() {
+  return {
+    nodeOps,
+    patchProp,
+  };
+}
